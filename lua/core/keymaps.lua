3f4cb09ecf855fc93e8414644058050ab8de2674
YvonRun <berteuil.yvon@gmail.com>
2023-05-04 07:59:55 +0000

    Initial commit

---
+++ lua/core/keymaps.lua
@@ -0,0 +1,25 @@
+vim.g.mapleader = ' ' 
+vim.g.maplocalleader = ' '
+
+vim.wo.number = true
+vim.wo.relativenumber = true
+
+
+vim.opt.backspace = '2'
+vim.opt.showcmd = true
+vim.opt.laststatus = 2
+vim.opt.autowrite = true
+vim.opt.cursorline = true
+vim.opt.autoread = true
+
+-- use spaces for tabs
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.shiftround = true
+vim.opt.expandtab = true
+
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
